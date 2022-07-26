cf09cdc2a2bbfe113453478c09c06848bf9ea930
Aleksey Pushkalov <pushkalov86@gmail.com>
2022-07-26 20:19:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write your code here
+const urlList
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+const url = 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
+const body = document.querySelector('body');
+const ul = document.createElement('ul');
+
+body.append(ul);
+
+const getPhones = () => {
+  fetch(urlList)
+    .then(response => {
+      setTimeout(() => {
+        if (!response.ok) {
+          throw new Error('Fetch failed');
+        }
+      }, 5000);
+
+      return response.json();
+    })
+    .then(phonesList => {
+      getPhonesDetails(phonesList);
+    });
+};
+
+const getPhonesDetails = (elems) => {
+  for (const el of elems) {
+    fetch(url + el.id + '.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch ${el.id} details`);
+        }
+
+        return response.json();
+      }).then(res => {
+        const li = document.createElement('li');
+
+        li.innerText = res.name;
+        ul.append(li);
+      });
+  }
+};
+
+getPhones();
